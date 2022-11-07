a99c7471d6917f765b701522d0dc483f6317e343
MoFares7 <faresdabbas1@gmail.com>
2022-11-07 21:28:34 +0200

    check is Wall or Empty

--- src/structure/Utils.java
+++ src/structure/Utils.java
@@ -34,20 +34,55 @@ public class Utils {
             // Print the row
             for(int j = 0; j < 16; j++)
             {
+                if(isWall(plan, row , col))
+                {
+                    plan[0][3] = plan[row][col];
+                    plan[row][col]='R';
 
-                plan[0][3] = plan[row][col];
-                plan[row][col]='R';
+                    System.out.print(plan[i][j] + " ");
+                    plan[row][col] = '_';
+                }
+                if(isEmpty(plan , row , col))
+                {
 
-                System.out.print(plan[i][j] + " ");
-                plan[row][col] = '_';
+                    plan[1][10] = plan[row][col];
+                   // plan[1][11] = plan[row][col];
+                    plan[row + 1][col]='R';
+
+                    System.out.print(plan[i][j] + " ");
+                    plan[row+1][col] = '_';
+                }
+
             }
-
 
             System.out.println();
             System.out.println();
         }
     }
 
+    public static boolean isWall(char[][] plan , int row, int col) {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 16; j++) {
+                if (plan[row][col] == '_') {
+                    return true;
+                }
+
+            }
+
+        }
+        return false;
+    }    public static boolean isEmpty(char[][] plan , int row, int col) {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 16; j++) {
+                if (plan[row][col] == ' ') {
+                    return true;
+                }
+
+            }
+
+        }
+        return false;
+    }
     public void nextSteps(){
 
     }
