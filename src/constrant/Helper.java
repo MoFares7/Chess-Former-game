625fcf834df54570a302bf5a51e512b14603cdc7
MoFares7 <faresdabbas1@gmail.com>
2022-11-08 20:40:50 +0200

    build Plan and contorl in all movment

--- src/constrant/Helper.java
+++ src/constrant/Helper.java
@@ -4,4 +4,7 @@ public class Helper {
 
    public int getNumHoraizonal = 16;
    public   int getNumVertical = 3;
+   public   int getNumPropriety = 100;
+   public   int getRowFinalState = 2;
+   public   int getColumnFinalState = 7;
 }
