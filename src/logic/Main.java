bda326990eea284472bc3973ae5740f5c4438a74
MoFares7 <faresdabbas1@gmail.com>
2022-11-13 23:00:06 +0200

    addition step isWin

--- src/logic/Main.java
+++ src/logic/Main.java
@@ -5,26 +5,19 @@ import game.Color;
 import game.Plan;
 import structure.Utils;
 
-import java.util.Scanner;
-
 public class Main
 {
-
-
     public static void main(String[] args)
     {
+
         Border border = new Border();
         Utils utils = new Utils();
         Plan plan = new  Plan();
-        Color color = new Color();
-        System.out.println();
-        System.out.println(color.RED + "Welcome in Chess Former Game Ready");
-        System.out.println();
-
 
   // initial border and print
        border.printBorder(plan.plan);
-       utils.getInput(plan.plan);
+
+       utils.getUserPlay(plan.plan);
 
     }
 }
