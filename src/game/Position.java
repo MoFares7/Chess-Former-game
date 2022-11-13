bfe9ad196cb0e24b5bbcafccb5d34c828925a0ff
MoFares7 <faresdabbas1@gmail.com>
2022-11-13 23:00:19 +0200

    addition step isWin

--- src/game/Position.java
+++ src/game/Position.java
@@ -1,19 +1,16 @@
 package game;
 
-public class Position {
-   int x;
-   int y;
+class Positions {
+    int x;
+    int y;
 
-  public Position(int x,int y) {
-         this.x = x;
-         this.y = y;
-   }
+    Positions(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
 
-   boolean isValid(){
-      if(y > -1 && y< 10 && x < 5 && x > -1)
-      {
-          return  true;
-      }
-      return false;
-   }
+    boolean isValid() {
+        return y > -1 && y < 10 && x < 16 && x > -1;
+    }
+
 }
