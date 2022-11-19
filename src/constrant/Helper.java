3ab4a3d58c6cdcbb845ec280a8d38ea367d99c6b
MoFares7 <faresdabbas1@gmail.com>
2022-11-19 21:19:48 +0200

    addition step isWin

--- src/constrant/Helper.java
+++ src/constrant/Helper.java
@@ -1,9 +1,19 @@
 package constrant;
 
+import structure.DFS;
+
 public class Helper {
 
    public int getNumHorizontal = 16;
    public   int getNumVertical = 3;
    public   int getNumPropriety = 100;
+
+
+   public void show(){
+      DFS.addEdge(0, 12);
+      DFS.addEdge(12, 1);
+      DFS.addEdge(1, 2);
+      DFS.addEdge(2, 7);
+   }
 
 }
