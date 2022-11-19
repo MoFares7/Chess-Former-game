a8456b42d8caa01a8012e0b9bdca4707ad4a52ac
MoFares7 <faresdabbas1@gmail.com>
2022-11-19 22:21:30 +0200

    addition step isWin

--- src/logic/Main.java
+++ src/logic/Main.java
@@ -1,8 +1,10 @@
 package logic;
 
+import constrant.Helper;
 import game.Border;
 import game.Color;
 import game.Plan;
+import structure.DFS;
 import structure.Utils;
 
 public class Main
@@ -14,10 +16,12 @@ public class Main
         Utils utils = new Utils();
         Plan plan = new  Plan();
 
-  // initial border and print
-      border.printBorder(plan.plan);
+        // initial border and print
+        border.printBorder(plan.plan);
 
-       utils.getUserPlay(plan.plan);
-
+        DFS.initializeDFS();
+        //  utils.getUserPlay(plan.plan);
     }
+
+
 }
