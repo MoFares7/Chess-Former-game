1bf729003648988939f345a4fc50ef8dc53c1f33
MoFares7 <faresdabbas1@gmail.com>
2022-11-20 13:22:51 +0200

    addition step isWin

--- src/constrant/Helper.java
+++ src/constrant/Helper.java
@@ -1,5 +1,6 @@
 package constrant;
 
+import structure.BFS;
 import structure.DFS;
 
 public class Helper {
@@ -9,11 +10,21 @@ public class Helper {
    public   int getNumPropriety = 100;
 
 
-   public void show(){
+   public void pathOfDFS(){
       DFS.addEdge(0, 12);
-      DFS.addEdge(12, 1);
+      DFS.addEdge(12,1);
       DFS.addEdge(1, 2);
+      DFS.addEdge(2, 2);
       DFS.addEdge(2, 7);
    }
+   public void pathOfBFS(){
+      BFS BFS = new BFS(160);
+      BFS.addEdge(0, 12);
+      BFS.addEdge(12,1);
+      BFS.addEdge(1, 2);
+      BFS.addEdge(2, 2);
+      BFS.addEdge(2, 7);
+   }
+
 
 }
