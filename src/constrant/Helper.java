e8af85add91c21e463f718873fe1cfac7376069d
MoFares7 <faresdabbas1@gmail.com>
2022-11-21 23:46:11 +0200

    add BFS algo

--- src/constrant/Helper.java
+++ src/constrant/Helper.java
@@ -1,26 +1,80 @@
 package constrant;
 
-import game.Border;
-import game.Plan;
-import structure.BFS;
-import structure.DFS;
-import structure.Utils;
 
-import java.util.Scanner;
+
+import static structure.DFS.addEdge;
 
 public class Helper {
 
    public int getNumHorizontal = 16;
-   public   int getNumVertical = 3;
-   public   int getNumPropriety = 100;
+   public int getNumVertical = 3;
+   public int getNumPropriety = 100;
+   public int source = 0, dest = 55;
 
-   Border border = new Border();
-   Utils utils = new Utils();
-   Plan plan = new  Plan();
-
-   Scanner scanner = new Scanner(System.in);
-
-
+   public void path(){
+      addEdge(0, 1);
+      addEdge( 0, 2);
+      addEdge( 0, 3);
+      addEdge( 0, 4);
+      addEdge( 0, 5);
+      addEdge( 0, 6);
+      addEdge( 0, 7);
+      addEdge( 0, 8);
+      addEdge( 0, 9);
+      addEdge( 0, 10);
+      addEdge(0, 11);
+      addEdge( 0, 12);
+      addEdge( 0, 13);
+      addEdge( 0, 14);
+      addEdge( 0, 15);
+      addEdge( 12, 16);
+      addEdge( 12, 17);
+      addEdge( 12, 18);
+      addEdge( 12, 19);
+      addEdge( 12, 20);
+      addEdge( 12, 21);
+      addEdge( 12, 22);
+      addEdge( 12, 23);
+      addEdge( 12, 24);
+      addEdge( 12, 25);
+      addEdge( 12, 26);
+      addEdge( 12, 27);
+      addEdge( 12, 28);
+      addEdge( 12, 29);
+      addEdge( 12, 30);
+      addEdge( 12, 31);
+      addEdge( 12, 32);
+      addEdge( 18, 33);
+      addEdge( 18, 34);
+      addEdge( 18, 35);
+      addEdge( 18, 36);
+      addEdge( 18, 37);
+      addEdge( 18, 38);
+      addEdge( 18, 39);
+      addEdge( 18, 40);
+      addEdge( 18, 41);
+      addEdge( 18, 42);
+      addEdge( 18, 43);
+      addEdge( 18, 44);
+      addEdge( 18, 45);
+      addEdge( 18, 46);
+      addEdge( 18, 47);
+      addEdge( 18, 48);
+      addEdge( 40, 49);
+      addEdge( 40, 50);
+      addEdge( 40, 51);
+      addEdge( 40, 52);
+      addEdge(40, 53);
+      addEdge( 40, 54);
+      addEdge( 40, 55);
+      addEdge( 40, 56);
+      addEdge( 40, 57);
+      addEdge(40, 58);
+      addEdge( 40, 59);
+      addEdge( 40, 60);
+      addEdge( 40, 61);
+      addEdge( 40, 62);
+   }
 
 
 
