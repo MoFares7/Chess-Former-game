1535df683796dd9fb7b4b309b576a72f005fd69b
MoFares7 <faresdabbas1@gmail.com>
2022-11-21 18:29:30 +0200

    add DFS algo

--- src/constrant/Helper.java
+++ src/constrant/Helper.java
@@ -21,35 +21,7 @@ public class Helper {
    Scanner scanner = new Scanner(System.in);
 
 
-   public void chooseModeGame()
-   {
-      System.out.println("Please Enter 1 to UserPlayer Mode");
-      System.out.println("Please Enter 2 to DFS Player Mode");
-      System.out.println("Please Enter 3 to BFS Player Mode");
-      System.out.print("The Mode game is : " + " ");
-      int number = scanner.nextInt();
 
-      switch (number)
-      {
-         case 1:
-         {
-            border.printBorder(plan.plan);
-            utils.getUserPlay(plan.plan);
-            break;
-         }
-         case 2:
-         {
-            DFS.initializeDFS();
-            break;
-         }
-         case 3:
-         {
-            BFS.initializeBFS();
-            break;
-         }
-         default:System.out.println("Invalid Value !");
-      }
-   }
 
 
 }
