1eb3cd54ffc4be31b5c6837448c0ee86cda89c8e
MoFares7 <faresdabbas1@gmail.com>
2022-11-21 23:45:32 +0200

    add DFS algo

--- src/structure/Utils.java
+++ src/structure/Utils.java
@@ -177,4 +177,23 @@ public class Utils {
         }
         return  false;
     }
-}
+ /*
+    @Override
+    public boolean equals(Object obj) {
+     if (this == obj)
+         return true;
+     if (obj == null)
+         return false;
+     if (getClass() != obj.getClass())
+         return false;
+     Vertex other = (Vertex) obj;
+     if (id == null) {
+         if (other.id != null)
+             return false;
+     } else if (!id.equals(other.id))
+         return false;
+     return true;
+
+  */
+ }
+
