619cb65f496b73ae96e5156e278bfeac09fd4626
MoFares7 <faresdabbas1@gmail.com>
2022-11-21 18:27:18 +0200

    add DFS algo

---
+++ src/game/Positions.java
@@ -0,0 +1,17 @@
+package game;
+
+public class Positions {
+        int x;
+        int y;
+
+        Positions(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+
+        boolean isValid() {
+            return y > -1 && y < 10 && x < 16 && x > -1;
+        }
+
+    }
+
