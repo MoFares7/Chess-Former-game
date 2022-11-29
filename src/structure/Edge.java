6dc3034399510dc9f85316b81419f18694fc8e30
MoFares7 <faresdabbas1@gmail.com>
2022-11-29 21:53:53 +0200

    add new Structure algo

---
+++ src/structure/Edge.java
@@ -0,0 +1,11 @@
+package structure;
+
+public class Edge {
+    public final double cost;
+    public final Node target;
+
+    public Edge(Node targetNode, double costVal) {
+        target = targetNode;
+        cost = costVal;
+    }
+}
